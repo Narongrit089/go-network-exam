81896aa498883588435764ac4af1597e6cf3249d
Narongrit089 <641463014@crru.ac.th>
2023-12-12 13:24:00 +0700

    update test

--- exam-1/main.go
+++ exam-1/main.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-
 func main() {
 	// TODO: Implement a simple Go program
 	fmt.Println(HelloFunction())
 }
 
 func HelloFunction() string {
-	return "Hello, Go!"
+	return "Hello"
 }
--- exam-1/main_test.go
+++ exam-1/main_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestExampleFunction(t *testing.T) {
-    // TODO: Implement tests for the student's code
-    // Example test (adjust according to the actual assignment)
-    expected := "Hello, Go!"
-    if got := HelloFunction(); got != expected {
-        t.Errorf("HelloFunction() = %v, want %v", got, expected)
-    }
+	// TODO: Implement tests for the student's code
+	// Example test (adjust according to the actual assignment)
+	expected := "Hello"
+	if got := HelloFunction(); got != expected {
+		t.Errorf("HelloFunction() = %v, want %v", got, expected)
+	}
 }

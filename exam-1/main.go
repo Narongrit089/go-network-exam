c8b5c20164fa3d7f78945daff371fa7161d2f4b0
Narongrit089 <641463014@crru.ac.th>
2023-12-12 14:04:35 +0700

    git commit -m

--- exam-1/main.go
+++ exam-1/main.go
@@ -3,10 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Implement a simple Go program
-	fmt.Println(HelloFunction())
+	// Got Username and Password from keyboard
+	var username string
+	var password string
+
+	fmt.Print("Username: ")
+	fmt.Scanln(&username)
+	fmt.Print("Password: ")
+	fmt.Scanln(&password)
+	// Call function LogIn
+
+	LogIn(username, password)
 }
 
 func HelloFunction() string {
 	return "Hello"
+}
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Minus(a int, b int) int {
+	return a - b
+}
+
+func UserLogin(username string, password string) bool {
+	if username == "admin" && password == "password" {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Use function UserLogin
+func LogIn(username string, password string) {
+	if UserLogin(username, password) {
+		fmt.Println("Login successful")
+	} else {
+		fmt.Println("Login failed")
+	}
 }
--- exam-1/main_test.go
+++ exam-1/main_test.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestExampleFunction(t *testing.T) {
 	// TODO: Implement tests for the student's code
 	// Example test (adjust according to the actual assignment)
-	expected := "Hello, Go!"
+	expected := "Hello"
 	if got := HelloFunction(); got != expected {
 		t.Errorf("HelloFunction() = %v, want %v", got, expected)
 	}
@@ -17,5 +15,19 @@ func TestAdd(t *testing.T) {
 	expected := 5
 	if got := Add(2, 3); got != expected {
 		t.Errorf("Add(2, 3) = %v, want %v", got, expected)
+	}
+}
+
+func TestMinus(t *testing.T) {
+	expected := 1
+	if got := Minus(3, 2); got != expected {
+		t.Errorf("Minus(3, 2) = %v, want %v", got, expected)
+	}
+}
+
+func TestUserLogin(t *testing.T) {
+	expected := true
+	if got := UserLogin("admin", "password"); got != expected {
+		t.Errorf("UserLogin(\"admin\", \"admin\") = %v, want %v", got, expected)
 	}
 }
